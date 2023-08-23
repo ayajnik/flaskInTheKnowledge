574afc1cf2338ab9560fdf41f999f81e04d4c09f
ayush yajnik <ayush.yajnik@yahoo.com>
2023-08-22 20:56:35 -0400

    flask basic started

---
+++ flask/flask_basics/flaskRoutes.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello puppy</h1>"
+
+@app.route('/information')
+def info():
+	return "<h1>puppies are cute and awesome</h1>"
+    
+if __name__=="__main__":
+    app.run()
