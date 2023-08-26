fbd613e46f1101b2455dfa7f4b4749cdc81e257a
ayush yajnik <ayush.yajnik@yahoo.com>
2023-08-26 08:32:01 -0400

    user views and template pt 1

--- companyblog/__init__.py
+++ companyblog/__init__.py
@@ -1,37 +1,40 @@
+# puppycompanyblog/__init__.py
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
 from flask_login import LoginManager
 
-###############################
-######### app ################
-##############################
+
 app = Flask(__name__)
 
-###############################
-########## db #################
-###############################
+
+############################
+### DATABASE SETUP ##########
+########################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app,db)
-basedir = os.path.abspath(os.path.dirname(__name__))
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_DATABASE_MODIFICATIONS'] = False
 
-
-################################
-########### Login ##############
-################################
-
+#########################
+# LOGIN CONFIGS
 login_manager = LoginManager()
+
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 
-from companyblog.core.views import core
-from companyblog.error_pages.views import error_handler
+
+
+##################################################
+
+
+from puppycompanyblog.core.views import core
+from puppycompanyblog.users.views import users
+from puppycompanyblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
-app.register_blueprint(error_handler)
-
-
+app.register_blueprint(users)
+app.register_blueprint(error_pages)
